52e0f23ed71fe5556fcf483ab076435a7fe5ad2a
Julian Berman <Julian@GrayVines.com>
2017-09-10 10:59:20 -0400

    Raising the roof. Incredible.

--- diff/_diff.py
+++ diff/_diff.py
@@ -3,8 +3,14 @@ import attr
 
 @attr.s
 class NotEqual(object):
+
     one = attr.ib()
     two = attr.ib()
+
+    def __bool__(self):
+        return False
+
+    __nonzero__ = __bool__
 
 
 def diff(one, two):
--- diff/tests/test_diff.py
+++ diff/tests/test_diff.py
@@ -7,3 +7,7 @@ class TestDiff(TestCase):
     def test_no_specific_diff_info(self):
         one, two = object(), object()
         self.assertEqual(diff.diff(one, two), diff.NotEqual(one, two))
+
+    def test_nonequality_is_falsy(self):
+        one, two = object(), object()
+        self.assertFalse(diff.NotEqual(one, two))
