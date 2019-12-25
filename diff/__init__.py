fca2fe60cb53ccd4c298cea8a8f2b9dddc6d1852
Julian Berman <Julian@GrayVines.com>
2019-12-25 12:11:41 +0100

    Badges and metadata.

--- diff/__init__.py
+++ diff/__init__.py
@@ -1,7 +1,7 @@
-from pkg_resources import get_distribution, DistributionNotFound
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # pragma: no cover
-    pass
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
 
 from diff._diff import Constant, Difference, diff
+__version__ = metadata.version("diff")
